7a2454dd66471126964bf74181a364a9905fa203
Alan <abx121xl@proton.me>
2026-01-05 16:59:19 +0700

    5 Jan 2026

--- JS/main.js
+++ JS/main.js
@@ -56,3 +56,106 @@ while (counter < 10) {
 for (let index = 0; index < 10; index++) {
   console.log("Hello World from for loop");
 }
+
+// let ages = [25, 30, 35, 40, 45];
+// console.log("Ages:", ages);
+
+// let fruits = ["apple", "banana", "cherry"];
+// console.log("Fruits:", fruits);
+
+// fruits.push("Mango");
+
+// console.log("Fruits after push:", fruits);
+
+// console.log("First fruit:", fruits.length);
+
+// console.log("All element in fruits array");
+// fruits.forEach((element, index) => {
+//   console.log(index, element);
+// });
+
+// let student = [
+//   { name: "John", age: 18, grade: "A" },
+//   { name: "Jane", age: 18, grade: "B" },
+// ];
+
+// // console.log("All keys of object:", Object.keys(student));
+// student.forEach((element, index) => {
+//   console.log("Name", element.name);
+//   console.log("Age", element.age);
+//   console.log("Grade", element.grade);
+//   if (index + 1 != student.length) {
+//     console.log("--------------------");
+//   }
+// });
+// student.push({ name: "Mike", age: 28, grade: "C" });
+// console.log("Added new student:", student[student.length - 1]);
+// student.pop();
+// console.log("Removed last student. Current students:", student);
+
+// let score1 = 10;
+// let score2 = 80;
+
+// function calculateGrade(score) {
+//   if (score < 0 || score > 100) {
+//     return "Invalid score";
+//   }
+
+//   if (score >= 90) {
+//     return "A";
+//   } else if (score >= 80) {
+//     return "B";
+//   } else if (score >= 70) {
+//     return "C";
+//   } else if (score >= 60) {
+//     return "D";
+//   } else {
+//     return "F";
+//   }
+// }
+
+// console.log("Score1:", score1, ", Grade:", calculateGrade(score1));
+// console.log("Score2:", score2, ", Grade:", calculateGrade(score2));
+
+let scores = [90, 80, 70, 60, 50];
+// let newScores = [];
+
+// for (let i = 0; i < scores.length; i++) {
+//   console.log(scores[i]);
+//   if (scores[i] >= 60) {
+//     newScores.push(scores[i]);
+//   }
+// }
+
+// for (let i = 0; i < scores.length; i++) {
+//   console.log(`Score ${i + 1}: ${scores[i]}`);
+// }
+
+// console.log("------------------------------");
+// scores = scores.map((s) => s - 10);
+
+// scores.forEach((s, i) => {
+//   console.log(`Score ${i + 1}:`, s);
+// });
+
+// let newScores = scores.filter((score) => score >= 70);
+// console.log("new scores:", newScores);
+
+let students = [
+  { name: "Jim", age: 16, grade: "A" },
+  { name: "Sara", age: 15, grade: "B" },
+  { name: "John", age: 17, grade: "C" },
+  { name: "Maya", age: 16, grade: "D" },
+  { name: "Tom", age: 15, grade: "F" },
+];
+
+console.log("Students", students[0]);
+
+let student = students.find((s) => {
+  return s.name === "Jim";
+});
+let doubleScoreStudents = students.map((s) => ({ ...s, age: s.age * 2 }));
+let highGradeStudents = students.filter((s) => s.grade === "A");
+console.log("Found Student", student);
+console.log("Double Score Student", doubleScoreStudents);
+console.log("High Grade Student", highGradeStudents);
